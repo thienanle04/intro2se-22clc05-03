b584c12e4a0793d33e76fce82a014a1eb39dc18c
22127294 <hpnghia22@clc.fitus.edu.vn>
2024-11-19 10:01:07 +0700

    Add new modify's component and build function to update data of server without authourization

--- frontend/main.js
+++ frontend/main.js
@@ -1,17 +1,20 @@
 import { addComponent } from "./addComponent.js";
 import { deleteComponent } from "./deleteComponent.js";
+import { modifyComponent } from "./modifyComponent.js";
 
 export default {
     data() {
         return {
             isAddComponent: true,  // Hiển thị component thêm sách
             isDeleteComponent: false, // Hiển thị component xóa sách
+            isModifyComponent: false,
             books: [], // Danh sách sách hiện có
         };
     },
     components: {
         addComponent,
-        deleteComponent
+        deleteComponent,
+        modifyComponent
     },
     methods: {
         // Thêm sách vào danh sách
@@ -28,15 +31,26 @@ export default {
             } else {
                 console.log(`Book with ISBN "${isbn}" and Title "${title}" not found!`);
             }
+        },
+        modifyBook(updatedBook) {
+            const index = this.books.findIndex(book => book.ISBN === updatedBook.ISBN);
+            if (index !== -1) {
+                this.books[index] = { ...this.books[index], ...updatedBook };
+                console.log("Book updated:", updatedBook);
+            } else {
+                console.log(`Book with ISBN "${updatedBook.ISBN}" not found!`);
+            }
         }
     },
     template: `
 <div>
-    <button class="btn btn-primary mb-3" @click="isAddComponent = true; isDeleteComponent = false;">Add Book</button>
-    <button class="btn btn-danger mb-3" @click="isAddComponent = false; isDeleteComponent = true;">Delete Book</button>
+    <button class="btn btn-primary mb-3" @click="isAddComponent = true; isDeleteComponent = false; isModifyComponent = false;">Add Book</button>
+    <button class="btn btn-danger mb-3" @click="isAddComponent = false; isDeleteComponent = true; isModifyComponent = false;">Delete Book</button>
+    <button class="btn btn-danger mb-3" @click="isAddComponent = false; isDeleteComponent = false; isModifyComponent = true;">Modify Book</button>
 
     <addComponent v-if="isAddComponent" @submit-book="addBook" />
     <deleteComponent v-if="isDeleteComponent" @delete-book="deleteBook" />
+    <modifyComponent v-if="isModifyComponent" @modify-book="modifyBook" />
 </div>
 `
 };
