2a949478ecf675aef6213ea5846e7c3abc119c73
22127005 <lethienan1106@gmail.com>
2024-11-20 01:29:55 +0700

    login, signup, resolve backend dependencies problem, vue frontend server

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { comLogin } from './components/login';
+import { comSignUp } from './components/signup';
+</script>
+
+<template>
+  <component :is="currentComponent" @login-success=LoggedIn @go-login=goLogin @go-home=goHome @go-signup=goSignup></component>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            token: null,
+            currentComponent: 'comLogin', // Current component to display
+        }
+    },
+    provide() {
+        return {
+        }
+    },
+    computed: {
+    },
+    methods: {
+        LoggedIn(data) {
+            this.currentComponent = 'comHome';
+            this.user = data;
+        },
+        goLogin() {
+            this.currentComponent = 'comLogin';
+        },
+        goHome() {
+            this.currentComponent = 'comHome';
+        },
+        goSignup() {
+            this.currentComponent = 'comSignUp';
+        },
+    },
+    components: { comLogin, comSignUp },
+  }
+</script>
