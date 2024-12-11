da7dcb321ef823d0addee8b8e7423969044ba5e3
nttuyen22@clc.fitus.edu.vn <nttuyen22@clc.fitus.edu.vn>
2024-12-11 23:34:59 +0700

    order's manager, change order's status
    commit 54509cbd7b9eee32e9221486eb33019a95f2e409
    Author: nttuyen22@clc.fitus.edu.vn <nttuyen22@clc.fitus.edu.vn>
    Date:   Wed Dec 11 22:26:36 2024 +0700
    
        update order managerment get all orders, update order's status, filter orders
    
        order's information
        update order's status: pending/shipping/delivering
        filter orders: name, id, date, status
    
    commit c7776d96436bb4f19f0fecb327df456481af6828
    Author: nttuyen22@clc.fitus.edu.vn <nttuyen22@clc.fitus.edu.vn>
    Date:   Sat Dec 7 01:03:04 2024 +0700
    
        add orderManagement using fake data
    
        todo:
        getAllOrders() from API
        updateOrder()
    
    commit 66f5432a8dd409b003e47fd5f72060d0eb5ce511
    Author: 22127294 <hpnghia22@clc.fitus.edu.vn>
    Date:   Sun Dec 8 21:50:52 2024 +0700
    
        CRUD for category management
    
    commit 07b5d922a9e84b94992c1ae2dce382485f4d8e09
    Author: kietnguyen2003 <nguyengiakiet0987654321@gmail.com>
    Date:   Sun Dec 8 21:05:38 2024 +0700
    
        Update Create new Book with cloudinary
    
    commit 593f6f74befd4b3953d6cf39ff888251757e6191
    Author: kietnguyen2003 <nguyengiakiet0987654321@gmail.com>
    Date:   Sun Dec 8 20:51:18 2024 +0700
    
        Fix Update + Delete book handle
    
    commit 5b27dd29f06c67eae8483e2d16527f52c63c96a0
    Author: 22127294 <hpnghia22@clc.fitus.edu.vn>
    Date:   Sun Dec 8 21:10:03 2024 +0700
    
        merge main
    
    commit f8fc58a1a875fad078fac07834d12cb4eec05da2
    Author: kietnguyen2003 <nguyengiakiet0987654321@gmail.com>
    Date:   Sat Dec 7 23:32:38 2024 +0700
    
        Update Order handle
    
    commit ecd74df131fad75d9ee35af17a3bc738e4b98841
    Author: kietnguyen2003 <nguyengiakiet0987654321@gmail.com>
    Date:   Sat Dec 7 22:58:41 2024 +0700
    
        Update genre handle
    
    commit 9a697f763915c65855ef2dae15a257fd16179e38
    Author: kietnguyen2003 <nguyengiakiet0987654321@gmail.com>
    Date:   Sat Dec 7 22:49:02 2024 +0700
    
        CRUD genre handle
    
    commit 5a0cae88abf48b9cba15a1968f37ce35afb4d2a5
    Author: 22127125 <ndvhoang22@clc.fitus.edu.vn>
    Date:   Fri Dec 6 22:57:28 2024 +0700
    
        Order books
    
    commit d971cc45fd9a0956d859cad018ac08d6ff0acf0b
    Author: nttuyen22@clc.fitus.edu.vn <nttuyen22@clc.fitus.edu.vn>
    Date:   Sat Dec 7 01:03:04 2024 +0700
    
        add orderManagement using fake data
    
        todo:
        getAllOrders() from API
        updateOrder()

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -93,6 +93,12 @@ const router = createRouter({
           component: () => import('@/components/shop/modifyUserComp.vue'),
         },
         {
+          path: 'orderManagement',
+          name: 'OrderManagement',
+          meta: { requiresAuth: true, roles: ['admin'] },
+          component: () => import('@/components/shop/orderManagementComp.vue'),
+        },
+        {
           path: 'addCategory',
           name: 'AddNewCategory',
           meta: { requiresAuth: true, roles: ['admin'] },
@@ -105,7 +111,7 @@ const router = createRouter({
           component: () => import('@/components/shop/modifyCategoryComp.vue'),
         },
       ],
-    }
+    }    
   ],
 });
 
